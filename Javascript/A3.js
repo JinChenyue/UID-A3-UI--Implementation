3224e84a24b347053581083ddd3d0ea5bc4ea29a
jinChenyue <chenyuej@student.unimelb.edu.au>
2024-05-31 23:31:19 +1000

    update js and css

--- Javascript/A3.js
+++ Javascript/A3.js
@@ -1,3 +1,21 @@
+// Information for all the products
+let products = [
+    { name: "Pet Air Filter", price: "29.99", originalPrice: "", image: "A3 image/filter.svg", detailPage:"productDetail.html" },
+    { name: "S-XL Front Range No-Pull Dog Harness", price: "54.95", originalPrice: "$119.99", image: "A3 image/harness.svg", detailPage:"productDetail 2.html"},
+    { name: "Calming Dog Bed", price: "58.99", originalPrice: "", image: "A3 image/products/dog bed.svg" },
+    { name: "Grooming Kit", price: "52.99", originalPrice: "$64.99", image: "A3 image/products/grooming kit.svg"},
+    { name: "Pet Safety Seat Belt", price: "23.99", originalPrice: "$35.99", image: "A3 image/products/seat belt.svg"},
+    { name: "Pet Dryer", price: "149.99", originalPrice: "", image: "A3 image/products/Dryer.svg" },
+    { name: "Teeth Cleaning Toy", price: "17.99", originalPrice: "$20.99", image: "A3 image/products/Tooth cleaning toy.svg"},
+    { name: "Pet Safety Seat Belt", price: "23.99", originalPrice: "$35.99", image: "A3 image/products/seat belt 2.svg"},
+    { name: "Cordless Vacuum Cleaner", price: "74.99", originalPrice: "", image: "A3 image/products/cleaner.svg"},
+    { name: "Cordless Pet Clipper Grooming Kit", price: "49.99", originalPrice: "", image: "A3 image/products/clipper.svg"},
+];
+
+// using local storage for persisting data across multiple pages
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+
 // Floating window
 // Shopping cart 
 function toggleCart() {
@@ -7,6 +25,7 @@ function toggleCart() {
     } else {
         cartOverlay.style.display = 'flex';
     }
+    displayCartItems()
 }
 // Search area
 function toggleSearch() {
@@ -18,25 +37,16 @@ function toggleSearch() {
     }
 }
 
-
-
-
+function toggleReturn() {
+    const searchOverlay = document.getElementById('returnOverlay');
+    if (searchOverlay.style.display === 'flex') {
+        searchOverlay.style.display = 'none';
+    } else {
+        searchOverlay.style.display = 'flex';
+    }
+}
 
 // Javascript for product list page
-
-// Information for all the products
-let products = [
-    { name: "Pet Air Filter", price: "$29.99", originalPrice: "", image: "A3 image/filter.svg" },
-    { name: "S-XL Front Range No-Pull Dog Harness", price: "$54.95", originalPrice: "$119.99", image: "A3 image/harness.svg"},
-    { name: "Calming Dog Bed", price: "$58.99", originalPrice: "", image: "A3 image/products/dog bed.svg" },
-    { name: "Grooming Kit", price: "$52.99", originalPrice: "$64.99", image: "A3 image/products/grooming kit.svg"},
-    { name: "Pet Safety Seat Belt", price: "$23.99", originalPrice: "$35.99", image: "A3 image/products/seat belt.svg"},
-    { name: "Pet Dryer", price: "$149.99", originalPrice: "", image: "A3 image/products/Dryer.svg" },
-    { name: "Teeth Cleaning Toy", price: "$17.99", originalPrice: "$20.99", image: "A3 image/products/Tooth cleaning toy.svg"},
-    { name: "Pet Safety Seat Belt", price: "$23.99", originalPrice: "$35.99", image: "A3 image/products/seat belt 2.svg"},
-    { name: "Cordless Vacuum Cleaner", price: "$74.99", originalPrice: "", image: "A3 image/products/cleaner.svg"},
-    { name: "Cordless Pet Clipper Grooming Kit", price: "$49.99", originalPrice: "", image: "A3 image/products/clipper.svg"},
-];
 
 
 // Loading all the products in product list page.
@@ -46,31 +56,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     products.forEach(product => {
         const productCard = `
-            <div class="product-card">
-                <img src="${product.image}" alt="${product.name}" class="product-image">
-                <h3 class="product-name">${product.name}</h3>
-                <div class = "product-details">
-                    <p class="product-price">${product.price}<span class="original-price">${product.originalPrice}</span></p>
-                    <div class="product-action">
-                        <img src="A3 image/Hearts.svg" alt="like" class="product-icon">
-                        <img src="A3 image/Shopping-Cart.svg" alt="add to cart" class="product-icon">
-                    </div>
+        <a href="${product.detailPage}" class="product-card">
+            <img src="${product.image}" alt="${product.name}" class="product-image">
+            <h3 class="product-name">${product.name}</h3>
+            <div class="product-details">
+                <p class="product-price">$${product.price}<span class="original-price">${product.originalPrice}</span></p>
+                <div class="product-action">
+                    <img src="A3 image/Hearts.svg" alt="like" class="product-icon">
+                    <img src="A3 image/Shopping-Cart.svg" alt="add to cart" class="product-icon">
                 </div>
             </div>
+        </a>
         `;
         container.insertAdjacentHTML('beforeend', productCard);
     });
 });
 
 
-
-
-
-
 // JaveScript for product details page
-
-// Set up varibles
 document.addEventListener('DOMContentLoaded', function() {
+// Set up varibles    
     const thumbnails = document.querySelectorAll('.thumbnail');
     const mainImage = document.getElementById('main-image');
     const quantityInput = document.getElementById('quantity-input');
@@ -108,54 +113,188 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+function addToCart(productIndex) {
+    const product = products[productIndex];
+    const cartItem = cart.find(item => item.name === product.name);
+    const quantityInput = document.getElementById('quantity-input');
+    if (cartItem) {
+        cartItem.quantity = parseInt(quantityInput.value);
+        // cartItem.quantity += 1;
+    } else {
+        cart.push({ name: product.name, price: product.price, quantity: 1 });
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCartItems();
+}
+
+function displayCartItems() {
+    const cartItems = document.getElementById('cart-items-container');
+    cartItems.innerHTML = '';
+
+    let total = 0;
+
+    cart.forEach(item => {
+        total += item.price * item.quantity;
+
+        const cartItemDiv = document.createElement('div');
+        cartItemDiv.className = 'cart-item';
+
+        cartItemDiv.innerHTML = `
+            <img src="${products.find(p => p.name === item.name).image}" alt="${item.name}" class="item-image">
+            <div class="item-details">
+                <h4 class="item-name">${item.name}</h4>
+                <p class="item-price">$${item.price}</p>
+            </div>   
+            <div class="quantity-control">
+                    <button class="quantity-decrease">-</button>
+                    <input id="quantity-input" value="${item.quantity}">
+                    <button class="quantity-increase">+</button>
+            </div>`;
+        cartItems.appendChild(cartItemDiv);
+    });
+    document.getElementById('total-price').textContent ='$'+ total.toFixed(2);
+}
+
+// 'check out' action in shopping cart 
+function checkout() {
+    window.location.href = 'paymentPage.html';
+}
 
 
-// var: products cart
+function displayOrderSummary() {
+    const orderSummary = document.getElementById('order-summary-container');
+    orderSummary.innerHTML = '';
 
-// name(product detail page)---> cart(get info form products)
+    let total = 0;
 
-// add to cart--->shopping cart (get jnfo from cart)
-// check out--->order(checkout page) (get info cart)
-// pay now--->order summary(confirmation page) (get info from cart
+    cart.forEach(item => {
+        total += item.price * item.quantity;
 
-// empty cart 
+        const orderItemDiv = document.createElement('div');
+        orderItemDiv.className = 'order-item';
 
+        orderItemDiv.innerHTML = `
+        <img src="${products.find(p => p.name === item.name).image}" alt="${item.name}" class="item-image">
+        <div class="item-details">
+            <h4 class="item-name">${item.name}</h4>
+            <p class="item-price">$${item.price}</p>
+        </div>
+        <div class="quantity-control">
+            <button class="quantity-decrease">-</button>
+            <input id="quantity-input" value="${item.quantity}">
+            <button class="quantity-increase">+</button>
+        </div>
+        `;
 
+        orderSummary.appendChild(orderItemDiv);
+    });
 
-// add to cart--checkout---paynow
+    const totalDiv = document.getElementById('order-total')
 
-// add item to cart and store info in cart=[]
-addToCart()
+    totalDiv.innerHTML = `
+    <p>Total</p>
+    <p> $${total.toFixed(2)}</p>
+    `;
+}
 
-// get quantity for input
-// parseInt(quantityInput.value);
-
-// cart page
-displayCartItem()
-// checkout page
-displayOrderSummary ()
-// confirmation page
-displayConfirmationSummary()
-
-// HTML for display item
-
-// const cartItem =`
-// <div class="cart-item">
-//     <img src="${item.image}" alt="Product image" class="item-image">
-//     <div class="item-details">
-//         <h4 class="item-name">${item.name}</h4>
-//         <p class="item-price">${item.price}</p>
-//     </div>                        
-//     <div class="quantity-control">
-//         <button class="quantity-decrease">-</button>
-//         <input id="quantity-input" value="${item.quantity}">
-//         <button class="quantity-increase">+</button>
-//     </div>
-// </div>
-// `;
+// 'pay now' action in checkout page
+function payNow() {
+    window.location.href = 'confirmationPage.html';
+}
 
 
 
+function displayConfirmationSummary() {
+    const confirmationSummary = document.getElementById('confirmation-summary-container');
+    confirmationSummary.innerHTML = '';
 
-let cart = [];
+    let total = 0;
 
+    cart.forEach(item => {
+        total += item.price * item.quantity;
+
+        const confirmationItemDiv = document.createElement('div');
+        confirmationItemDiv.className = 'confirmation-item';
+
+        confirmationItemDiv.innerHTML = `
+        <img src="${products.find(p => p.name === item.name).image}" alt="${item.name}" class="item-image">
+        <div class="item-details">
+            <h4 class="item-name">${item.name}</h4>
+            <p class="item-price">$${item.price}</p>
+        </div>           
+        <div class="quantity-control">
+            <input id="quantity-input" value="${item.quantity}">
+        </div>
+
+        `;
+
+        confirmationSummary.appendChild(confirmationItemDiv);
+    });
+
+    const totalDiv = document.getElementById('confirmation-total')
+
+    totalDiv.innerHTML = `
+        <div class="summary">
+            <span><p><strong>Subtotal</strong> </p><p>$${total.toFixed(2)}</p></span>
+            <span><p><strong>Shipping</strong> </p><p>Free</p></span>                  
+        </div>
+        <span class="total"><p><strong>Total</strong></p> <p id="item-total">$${total.toFixed(2)}</p></span>
+    `;
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayCartItems();
+});
+
+function returnHomepage() {
+    window.location.href = 'index.html'
+    // Empty the cart after confirmation
+    cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
+
+// automatically fill the form 
+document.addEventListener('DOMContentLoaded', function() {
+    const autofillCheckbox = document.getElementById('autofill-checkbox');
+
+    autofillCheckbox.addEventListener('change', function() {
+        if (this.checked) {
+            document.getElementById('region').value = 'VICTORIA';
+            document.getElementById('first-name').value = 'Eva';
+            document.getElementById('last-name').value = 'Fang';
+            document.getElementById('company').value = '';
+            document.getElementById('address').value = '7 Haideh St';
+            document.getElementById('apartment').value = '';
+            document.getElementById('city').value = 'Wantirna South';
+            document.getElementById('state').value = 'Melbourne';
+            document.getElementById('postcode').value = '3152';
+            document.getElementById('email').value = 'abcgh@hotmail.com';
+            document.getElementById('phone').value = '0486379039';
+            document.getElementById('card-number').value = '1234 6354 8653 2738';
+            document.getElementById('security-code').value = '123';
+            document.getElementById('expiration').value = '12/04';
+            document.getElementById('name-on-card').value = 'Eva Fang';
+
+        } else {
+            document.getElementById('region').value = '';
+            document.getElementById('first-name').value = '';
+            document.getElementById('last-name').value = '';
+            document.getElementById('company').value = '';
+            document.getElementById('address').value = '';
+            document.getElementById('apartment').value = '';
+            document.getElementById('city').value = '';
+            document.getElementById('state').value = '';
+            document.getElementById('postcode').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('phone').value = '';
+            document.getElementById('card-number').value = '';
+            document.getElementById('security-code').value = '';
+            document.getElementById('expiration').value = '';
+            document.getElementById('name-on-card').value = '';
+        }
+    });
+});
